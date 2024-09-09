978399f3088d501a9c44f28e1aac4af09cb4af63
Sushil <sushilkumarskp618@gmail.com>
2024-09-09 22:09:42 +0530

    routing done

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreditsPage = () => {
+    return (
+        <div>
+            Credits page
+        </div>
+    )
+}
+
+export default CreditsPage
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationPage = () => {
+    return (
+        <div>
+            Transformations
+        </div>
+    )
+}
+
+export default TransformationPage
